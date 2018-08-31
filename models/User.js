7d93e3e90d00ed47b8efd9782a8ae79d8d090939
Rye-Guy <ryanracioppo95@gmail.com>
2018-08-31 18:08:14 -0400

    passwords are hashed now, but for some reason its not ES6 compatable so it only works it function and var

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -16,6 +17,18 @@ var UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+UserSchema.pre('save', function (next){
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash){
+        if(err){
+            return next(err);
+        }
+        user.password = hash;
+        next();
+        console.log(hash);
+    });
 });
 
 var User = mongoose.model('User', UserSchema);
