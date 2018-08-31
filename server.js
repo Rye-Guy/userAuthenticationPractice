36a1ac4e636cc5bf07375f083a88f3734747157f
Rye-Guy <ryanracioppo95@gmail.com>
2018-08-31 18:14:08 -0400

    added express-session...also Christine watch this https://www.youtube.com/watch?v=9PA-F6ip9uY

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ db.once('open', () =>{
     console.log('db connected!');
 });
 
+app.use(session({
+    secret: '911 was an inside job',
+    resave: true,
+    saveUninitialized: false
+}))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
